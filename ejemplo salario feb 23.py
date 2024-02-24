e6f3bc608bade9a8cc024ba4610ec63c118f5900
andrego151k <161092979+andrego151k@users.noreply.github.com>
2024-02-23 21:28:35 -0500

    Add files via upload

---
+++ ejemplo salario feb 23.py
@@ -0,0 +1,14 @@
+print ("\n\t\t\t    bienvenido tu calculadora de trabajo online")
+nombre = input("\n    ingrese nombre: ")
+ht = int(input("   ingrese horas : "))
+vh = float(input("    valor hora: "))
+SS = ht*vh
+#f-sting
+mensaje = (f'tu nombre es:{nombre} \t horas tabajadas: {ht} '
+           f'\n valor hora: {vh} ')
+print ("Los datos que has registrado \n hasta ahora son: ")
+print(mensaje)
+print(SS)
+
+'''semanatrabajada = int(input("    semanas trabajadas: "))#
+#VT = 10000 HT = 47 SS = VT*HT#'''
